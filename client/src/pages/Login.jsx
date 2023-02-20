79afce3fb4480902d9c1c2efd423fff98e091f46
codepanther <pezechigozirim@gmail.com>
2023-02-20 10:32:57 +0100

    Implement register and login feature

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,14 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+  const [formInput, setFormInput] = useState({
+    username: '',
+    password: ''
+  });
+  const [redirect, setRedirect] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormInput({ ...formInput, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { username, password } = formInput;
+    const res = await fetch('http://localhost:5000/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ username, password }),
+      credentials: 'include'
+    });
+
+    if (res.ok) setRedirect(true);
+  };
+
+  if (redirect) return navigate('/');
+
   return (
-    <form className="login">
+    <form onSubmit={handleSubmit} className="login">
       <h1>Login</h1>
-      <input type="text" placeholder="username" />
-      <input type="password" placeholder="password" />
+      <input
+        type="text"
+        name="username"
+        placeholder="username"
+        onChange={handleChange}
+      />
+      <input
+        type="password"
+        name="password"
+        placeholder="password"
+        onChange={handleChange}
+      />
       <button type="submit">Login</button>
     </form>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Register = () => {
+  const [formInput, setFormInput] = useState({
+    username: '',
+    password: ''
+  });
+
+  const handleChange = (e) => {
+    setFormInput({ ...formInput, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    const { username, password } = formInput;
+    e.preventDefault();
+    const res = await fetch('http://localhost:5000/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ username, password })
+    })
+
+    if (!res.ok) {
+      alert('registration failed')
+      return;
+    }
+
+    alert('registration successful');
+  }
+
   return (
-    <form className="register">
+    <form onSubmit={handleSubmit} className="register">
       <h1>Register</h1>
-      <input type="text" placeholder="username" />
-      <input type="password" placeholder="password" />
+      <input
+        type="text"
+        name="username"
+        placeholder="username"
+        onChange={handleChange}
+      />
+      <input
+        type="password"
+        name="password"
+        placeholder="password"
+        onChange={handleChange}
+      />
       <button type="submit">Register</button>
     </form>
   );
